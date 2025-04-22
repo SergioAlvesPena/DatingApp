f0d9dc4e0ca6299a72ed72ae8c03fd808501f506
Sergio Pena <sergio.penaneto@gmail.com>
2025-04-22 17:32:08 -0300

    Added signalR to the api refactored Program.cs and ApplicationServiceExtensions.cs to work with SignalR

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200/"));
 
 if (app.Environment.IsDevelopment())
@@ -31,6 +32,8 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<PresenceHub>("hub/presence");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
